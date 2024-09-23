a8af8308c1d8776d3db9ac28afd5c6fd2f305031
simeon.gor <simeon.gor@yandex.ru>
2024-09-23 15:21:23 +0300

    feat: add request validation

--- src/main/java/com/simeon/Main.java
+++ src/main/java/com/simeon/Main.java
@@ -1,16 +1,5 @@
 package com.simeon;
 
-import com.fastcgi.FCGIInterface;
-import com.fastcgi.FCGIRequest;
-import com.simeon.dto.Request;
-import com.simeon.dto.Response;
-import com.simeon.dto.ResponseEntity;
-import com.simeon.view.ViewResolver;
-
-import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.util.HashMap;
-
 public class Main {
     public static void main(String[] args) {
         Application application = new Application();
